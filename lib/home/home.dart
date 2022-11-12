99b0e764c01b9eb68ff208d06632a794f8e74ba9
Billy Chan <billychan223@gmail.com>
2022-11-12 16:08:45 +0800

    refactor: update firebase config and root view structure

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,12 +1,53 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import '../portfolio/portfolio.dart';
+import '../dashboard/dashboard.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _TabContent {
+  final String tabName;
+  final Widget child;
+  final Widget icon;
+
+  const _TabContent({required this.tabName, required this.child, required this.icon});
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  static const List<_TabContent> homeTab = [
+    _TabContent(tabName: 'Dashboard', child: Dashboard(), icon: Icon(Icons.home)),
+    _TabContent(tabName: 'Portfolio', child: Portfolio(), icon: Icon(Icons.person))
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onTabSelected(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: homeTab.map((tab) => tab.child).toList(growable: false),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: homeTab
+            .map((tab) => BottomNavigationBarItem(
+                  icon: tab.icon,
+                  label: tab.tabName,
+                ))
+            .toList(growable: false),
+        currentIndex: _selectedIndex,
+        onTap: _onTabSelected,
+      ),
+    );
   }
 }
---
+++ lib/portfolio/portfolio.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Portfolio extends StatelessWidget {
+  const Portfolio({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(child: Text('Portfolio')),
+      color: Colors.white,
+    );
+  }
+}
