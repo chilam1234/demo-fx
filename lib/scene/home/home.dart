dc2b94959d74dd0756dae7f922518172b060a8af
Billy Chan <billychan223@gmail.com>
2022-11-15 23:36:59 +0800

    feat: logni page

--- lib/scene/home/home.dart
+++ lib/scene/home/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import '../../service/auth.dart';
+import '../../shared_widget/error.dart';
+import '../../shared_widget/loading.dart';
+import '../login/login.dart';
 import '../portfolio/portfolio.dart';
 import '../dashboard/dashboard.dart';
 
@@ -14,13 +18,16 @@ class _TabContent {
   final Widget child;
   final Widget icon;
 
-  const _TabContent({required this.tabName, required this.child, required this.icon});
+  const _TabContent(
+      {required this.tabName, required this.child, required this.icon});
 }
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   static const List<_TabContent> homeTab = [
-    _TabContent(tabName: 'Dashboard', child: Dashboard(), icon: Icon(Icons.home)),
-    _TabContent(tabName: 'Portfolio', child: Portfolio(), icon: Icon(Icons.person))
+    _TabContent(
+        tabName: 'Dashboard', child: Dashboard(), icon: Icon(Icons.home)),
+    _TabContent(
+        tabName: 'Portfolio', child: Portfolio(), icon: Icon(Icons.person))
   ];
 
   int _selectedIndex = 0;
@@ -33,21 +40,37 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: homeTab.map((tab) => tab.child).toList(growable: false),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: homeTab
-            .map((tab) => BottomNavigationBarItem(
-                  icon: tab.icon,
-                  label: tab.tabName,
-                ))
-            .toList(growable: false),
-        currentIndex: _selectedIndex,
-        onTap: _onTabSelected,
-      ),
-    );
+    return StreamBuilder(
+        stream: AuthService().userStream,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LoadingScreen();
+          } else if (snapshot.hasError) {
+            return const Center(
+              child: ErrorMessage(),
+            );
+          } else if (snapshot.hasData) {
+            return Scaffold(
+              body: IndexedStack(
+                index: _selectedIndex,
+                children:
+                    homeTab.map((tab) => tab.child).toList(growable: false),
+              ),
+              bottomNavigationBar: BottomNavigationBar(
+                items: homeTab
+                    .map((tab) => BottomNavigationBarItem(
+                          icon: tab.icon,
+                          label: tab.tabName,
+                        ))
+                    .toList(growable: false),
+                currentIndex: _selectedIndex,
+                onTap: _onTabSelected,
+              ),
+            );
+            ;
+          } else {
+            return const LoginScreen();
+          }
+        });
   }
 }
