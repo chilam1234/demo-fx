30e9adbd18c0b42ab3d941aa89d45e243eb1c5f5
Billy Chan <billychan223@gmail.com>
2022-11-23 23:47:44 +0800

    fix: remove portfolio tab

--- lib/scene/home/home.dart
+++ lib/scene/home/home.dart
@@ -3,7 +3,6 @@ import '../../service/auth.dart';
 import '../../shared_widget/error.dart';
 import '../../shared_widget/loading.dart';
 import '../login/login.dart';
-import '../portfolio/portfolio.dart';
 import '../dashboard/dashboard.dart';
 
 class Home extends StatefulWidget {
@@ -13,31 +12,7 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _TabContent {
-  final String tabName;
-  final Widget child;
-  final Widget icon;
-
-  const _TabContent(
-      {required this.tabName, required this.child, required this.icon});
-}
-
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
-  static const List<_TabContent> homeTab = [
-    _TabContent(
-        tabName: 'Dashboard', child: Dashboard(), icon: Icon(Icons.home)),
-    _TabContent(
-        tabName: 'Portfolio', child: Portfolio(), icon: Icon(Icons.person)),
-  ];
-
-  int _selectedIndex = 0;
-
-  void _onTabSelected(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
@@ -50,22 +25,8 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
               child: ErrorMessage(),
             );
           } else if (snapshot.hasData) {
-            return Scaffold(
-              body: IndexedStack(
-                index: _selectedIndex,
-                children:
-                    homeTab.map((tab) => tab.child).toList(growable: false),
-              ),
-              bottomNavigationBar: BottomNavigationBar(
-                items: homeTab
-                    .map((tab) => BottomNavigationBarItem(
-                          icon: tab.icon,
-                          label: tab.tabName,
-                        ))
-                    .toList(growable: false),
-                currentIndex: _selectedIndex,
-                onTap: _onTabSelected,
-              ),
+            return const Scaffold(
+              body: Dashboard()
             );
           } else {
             return const LoginScreen();
